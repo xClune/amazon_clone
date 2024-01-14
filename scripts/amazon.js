46ef9205723159ae821f65f1b43fac8131f113a4
AC <aclune382@gmail.com>
2024-01-14 15:24:27 +0000

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -78,6 +78,15 @@ document.querySelectorAll('.js-add-to-cart')
           productId: productId,
           quantity: 1
         })
-      }     
+      }   
+      
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      })
+      
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
     })
   });
