afdbe4a83146251306aa72930386e0179e395435
AC <aclune382@gmail.com>
2024-01-19 14:22:32 +0000

    Make added to cart message timer reset upon new button click.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -65,6 +65,8 @@ document.querySelector('.js-products-grid')
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
+    let addedMessageTimeoutId;
+
     button.addEventListener('click', () => {
       const {productId} = button.dataset;
       const quantitySelector = (document.querySelector(`.js-quantity-selector-${productId}`));
@@ -96,13 +98,28 @@ document.querySelectorAll('.js-add-to-cart')
       document.querySelector('.js-cart-quantity')
         .innerHTML = cartQuantity;
 
-      const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`)
+      // Added to cart message
+
+      const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`
+        );
       
       addedMessage.classList.add('added-to-cart-visible');
 
-      setTimeout(() => {
-        addedMessage.classList.remove('added-to-cart-visible');
-      }, 2000)
+      // Create timeout after 2 seconds
 
-    })
+    setTimeout(() => {
+      // check if previous timeoutId exists.
+      // if it does, stop it.
+      if (addedMessageTimeoutId) {
+        clearTimeout(addedMessageTimeoutId);
+      }
+
+      const timeoutId = setTimeout(() => {
+        addedMessage.classList.remove('added-to-cart-visible');
+      }, 2000);
+      
+
+      addedMessageTimeoutId = timeoutId;
+      });
+    });
   });
