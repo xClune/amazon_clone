d2ea30885696d2227ae439aa05fbffe4361fcb1d
AC <aclune382@gmail.com>
2024-01-26 15:25:51 +0000

    Save data and generate HTML for payment summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -24,4 +24,4 @@ export function getDeliveryOption(deliveryOptionId) {
     });
   
     return deliveryOption; 
-  }
+}
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,6 +1,6 @@
 // Named export examples
 import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 // Default export example 
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -15,23 +15,11 @@ export function renderOrderSummary() {
     cart.forEach((cartItem) => {
         const productId = cartItem.productId;
 
-        let matchingProduct;
-
-        products.forEach((product) => {
-            if (product.id === productId) {
-                matchingProduct = product;
-            }
-        });
+        const matchingProduct = getProduct(productId);
 
         const deliveryOptionId = cartItem.deliveryOptionId;
 
-        let deliveryOption;
-    
-        deliveryOptions.forEach((option) => {
-        if (option.id === deliveryOptionId) {
-            deliveryOption = option;
-        }
-        });
+        const deliveryOption = getDeliveryOption(deliveryOptionId);
 
         const today = dayjs();
         const deliveryDate = today.add(
