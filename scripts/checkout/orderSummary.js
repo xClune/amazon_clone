2d89b5c4ce8ca5798e9bb25a3d4a8daf9e6e7116
AC <aclune382@gmail.com>
2024-01-27 08:41:49 +0000

    Payment summary qty element updates automatically with cart changes

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -161,11 +161,12 @@ export function renderOrderSummary() {
             const productId = link.dataset.productId;
             removeFromCart(productId);
 
-            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            //const container = document.querySelector(`.js-cart-item-container-${productId}`);
             
-            container.remove();
+            //container.remove();
             
             updateCartHeader();
+            renderOrderSummary();
             renderPaymentSummary();
             });
             
